97f0b45b9160094defbbecf0643d7a5381906844
tukky-14 <bigoonage0104@gmail.com>
2023-06-07 22:18:12 +0900

    DataGridを追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './Home';
 import CardGrid from './pages/CardGrid';
+import DataGrid from './pages/DataGrid';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                 <Route index path="" element={<Home />} />
                 <Route path="/card-grid" element={<CardGrid />} />
+                <Route path="/data-grid" element={<DataGrid />} />
             </Routes>
         </BrowserRouter>
     );
--- src/Home.tsx
+++ src/Home.tsx
@@ -3,9 +3,12 @@ import './App.css';
 
 function Home() {
     return (
-        <div className="max-w-screen-xl mx-auto p-4">
+        <div className="max-w-screen-xl mx-auto p-4 flex flex-col gap-2">
             <Link to="/card-grid" className="text-xl text-blue-600 hover:underline">
                 CardGrid
+            </Link>
+            <Link to="/data-grid" className="text-xl text-blue-600 hover:underline">
+                DataGrid
             </Link>
         </div>
     );
