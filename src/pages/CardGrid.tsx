fad3b2b70fb06f1c8f1256538aef407ed2e4b9f1
tukky-14 <bigoonage0104@gmail.com>
2023-06-13 22:50:28 +0900

    CardGridをリファクタリング

--- src/pages/CardGrid.tsx
+++ src/pages/CardGrid.tsx
@@ -1,19 +1,30 @@
+const boxes = [
+    'box1',
+    'box2',
+    'box3',
+    'box4',
+    'box5',
+    'box6',
+    'box7',
+    'box8',
+    'box9',
+    'box10',
+    'box11',
+    'box12',
+];
+
 const CardGrid = () => {
     return (
         <div className="max-w-screen-xl mx-auto p-4">
-            <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-                <li className="border px-2 py-10 text-center shadow-md">Box</li>
-                <li className="border px-2 py-10 text-center shadow-md">Box</li>
-                <li className="border px-2 py-10 text-center shadow-md">Box</li>
-                <li className="border px-2 py-10 text-center shadow-md">Box</li>
-                <li className="border px-2 py-10 text-center shadow-md">Box</li>
-                <li className="border px-2 py-10 text-center shadow-md">Box</li>
-                <li className="border px-2 py-10 text-center shadow-md">Box</li>
-                <li className="border px-2 py-10 text-center shadow-md">Box</li>
-                <li className="border px-2 py-10 text-center shadow-md">Box</li>
-                <li className="border px-2 py-10 text-center shadow-md">Box</li>
-                <li className="border px-2 py-10 text-center shadow-md">Box</li>
-                <li className="border px-2 py-10 text-center shadow-md">Box</li>
+            <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+                {boxes.map((box) => (
+                    <li
+                        key={box}
+                        className="border px-2 py-10 text-center shadow-md hover:scale-110 duration-200"
+                    >
+                        {box}
+                    </li>
+                ))}
             </ul>
         </div>
     );
